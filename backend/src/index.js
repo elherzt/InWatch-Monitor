50f80e92736d76328822a35bce689d1e98141d12
Jesus Romero <elherzt@gmail.com>
2025-11-25 17:05:15 -0600

    add variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "" + env.CORS_ALLOWED_ORIGINS);
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") {
